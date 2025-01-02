7d63d6dd39473b147cdf732929ed29e7adeb13cb
IvashukR <rostikivasuk1@gmail.com>
2025-01-02 21:00:36 +0200

    Second mini commit

--- scripts/TownEnemy.cs
+++ scripts/TownEnemy.cs
@@ -4,13 +4,4 @@ using System;
 public partial class TownEnemy : StaticBody2D
 {
 	public int proch = 50;
-	public override void _Ready()
-	{
-
-	}
-
-	public override void _Process(double delta)
-	{
-
-	}
 }
