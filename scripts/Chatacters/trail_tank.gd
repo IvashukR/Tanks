324a89936e88c73317447beb70d6cb7f3d372f0b
IvashukR <rost87085@gmail.com>
2025-04-06 23:05:02 +0300

    trails tanks

--- scripts/Chatacters/trail_tank.gd
+++ scripts/Chatacters/trail_tank.gd
@@ -1,8 +1,14 @@
 extends Node2D
 @export var actor : CharacterBody2D
 @onready var t : Timer = $Timer
+@onready var trail1 = $trail
+var list_trails : Array = []
 var flag_move : bool = false
 func _ready() -> void:
+	for i in get_children():
+		if i is Timer:
+			break
+		list_trails.append(i)
 	pass
 	
 func _physics_process(_delta: float) -> void:
@@ -11,7 +17,12 @@ func _physics_process(_delta: float) -> void:
 		t.start()
 func s():
 	if get_child_count() == 0:
-		t.stop()
+		for i : Sprite2D in list_trails:
+			i.reparent(self, false)
+			i.global_position = global_position
+			i.global_rotation = global_rotation
+			await get_tree().create_timer(0.5).timeout
+			
 		return
 	get_child(0).reparent(get_tree().root)
 
@@ -19,4 +30,7 @@ func s():
 func _on_timer_timeout() -> void:
 	if actor.velocity.length() > 0:
 		s()
+	else:
+		flag_move = false
+		t.stop()
 	pass 
