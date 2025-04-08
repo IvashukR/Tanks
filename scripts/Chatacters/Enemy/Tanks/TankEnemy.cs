d76f1ee33be3090b43145f80d4ce8270a57affb2
IvashukR <rost87085@gmail.com>
2025-04-08 22:20:50 +0300

    update trail tank

--- scripts/Chatacters/Enemy/Tanks/TankEnemy.cs
+++ scripts/Chatacters/Enemy/Tanks/TankEnemy.cs
@@ -6,40 +6,51 @@ namespace Enemy.Transport;
 public partial class TankEnemy : CharacterBody2D
 {
     private NavigationAgent2D agent;
-    private Timer t_patrul;
+    private Timer t_patrul, t_attack;
     private Vector2 direction;
     public Sprite2D pushka { get; set; }
     private Sprite2D  osnova;
     [Export] private float speed;
-    [Export] private float rotation_time;
+    [Export] private PhysicsBody2D target;
+    private bool attack = false;
     public override void _Ready()
     {
         agent = GetNode<NavigationAgent2D>("%agent");
         pushka = GetNode<Sprite2D>("%bashnya");
         osnova = GetNode<Sprite2D>("%osnova");
         t_patrul = GetNode<Timer>("%t_patrul");
+        t_attack = GetNode<Timer>("%t_attack");
+        t_attack.Start();
         t_patrul.Start();
+        SetRandomDirection();
+        t_attack.Timeout += Attack;
         t_patrul.Timeout += SetRandomDirection;
     }
     public override void _PhysicsProcess(double delta)
     {
+        var next_pos = agent.GetNextPathPosition();
+        direction = GlobalPosition.DirectionTo(next_pos);
         Velocity = direction * speed * (float)delta;
+        var angle_target = direction.Angle();
+        Rotation = Mathf.LerpAngle(Rotation, angle_target, (float)delta * 0.4f);
         MoveAndSlide();
     }
-    private async void SetRandomDirection()
+    private void Attack()
+    {
+        SetTarget(target.GlobalPosition);
+        
+    }
+    private void SetTarget(Vector2 target_pos)
+    {
+        agent.TargetPosition = target_pos;
+    }
+    private void SetRandomDirection()
     {
         direction = Vector2.Zero;
         var new_direction = new Vector2(GD.RandRange(15, 230), GD.RandRange(15, 230));
         var distance = new_direction - GlobalPosition;
         if(distance.Length() < 1)return;
-        direction = distance.Normalized();
-        var target_angle = distance.Angle();
-        var tween = CreateTween();
-        tween.SetEase(Tween.EaseType.InOut);
-        tween.SetTrans(Tween.TransitionType.Sine);
-        tween.TweenProperty(this, "rotation", target_angle , rotation_time);
-        await ToSignal(tween, "finished");
-        direction = distance.Normalized();
+        SetTarget(new_direction);
     }
     
 }
--- scripts/Chatacters/trail_tank.gd
+++ scripts/Chatacters/trail_tank.gd
@@ -1,35 +1,41 @@
 extends Node2D
 @export var actor : CharacterBody2D
 @onready var t : Timer = $Timer
-@onready var trail1 = $trail
 var list_trails : Array = []
 var flag_move : bool = false
+var flag_hide : bool = false
 func _ready() -> void:
 	for i in get_children():
 		if i is Timer:
 			break
 		list_trails.append(i)
 	pass
-	
+
+func _process(_delta: float) -> void:
+	if !flag_hide and get_child_count() < 4:
+		flag_hide = true
+		hide_trails()
+		pass
 func _physics_process(_delta: float) -> void:
 	if actor.velocity.length() > 0 and not flag_move:
 		flag_move = true
 		t.start()
-func s():
+func show_trail() -> void:
 	if get_child_count() == 0:
-		for i : Sprite2D in list_trails:
-			i.reparent(self, false)
-			i.global_position = global_position
-			i.global_rotation = global_rotation
-			await get_tree().create_timer(0.5).timeout
-			
 		return
 	get_child(0).reparent(get_tree().root)
 
+func hide_trails() -> void:
+	for i : Sprite2D in list_trails:
+		i.reparent(self, false)
+		i.global_position = global_position
+		i.global_rotation = global_rotation
+		await get_tree().create_timer(0.5).timeout
+	flag_hide = false
 
 func _on_timer_timeout() -> void:
 	if actor.velocity.length() > 0:
-		s()
+		show_trail()
 	else:
 		flag_move = false
 		t.stop()
